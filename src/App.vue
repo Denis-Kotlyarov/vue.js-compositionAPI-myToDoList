639a714693f0f3e02ab05a489dbb2e8835f96681
Denis <hom3246@gmail.com>
2024-04-15 19:08:37 +0300

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Привет, composition API! Мы отлично сработаемся!</h1>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
